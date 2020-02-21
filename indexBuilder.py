c9985f5831e3cbeeff2d8afd56a4b166a50aed01
Allyson Yamasaki <allyson.yamasaki@gmail.com>
2020-02-21 14:09:27 -0800

    added structure of index builder using psuedo code from lec, not implemented yet

---
+++ indexBuilder.py
@@ -0,0 +1,14 @@
+# index builder
+class Posting:
+    def __init__(self, docid, tfidf, fields):
+        self.docid = docid
+        self.tfidf = tfidf  # use freqcounts for now
+        self.fields = fields
+
+    def __repr__(self):
+        return str.format("DOC ID: {} \nCOUNT : {} \nFIELDS: {}", self.docid, self.tfidf, self.fields)
+
+
+# TODO index construction procedure
+def build_index(txt_docs: set()) -> dict():
+    return dict()
