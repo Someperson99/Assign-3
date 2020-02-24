5e7441b537d21bf850c09d4038369997947ace51
Allyson Yamasaki <allyson.yamasaki@gmail.com>
2020-02-23 16:31:03 -0800

    new index builder

--- indexBuilder.py
+++ indexBuilder.py
@@ -1,38 +1,32 @@
 # index builder
 import re
-
-class Posting:
-    def __init__(self, docid, tfidf, fields=None):
-        self.docid = docid
-        self.tfidf = tfidf  # use freqcounts for now
-        self.fields = fields
-
-    def __repr__(self):
-        return str.format("DOC ID: {} \nCOUNT : {} \nFIELDS: {}", self.docid, self.tfidf, self.fields)
-
+from Posting import *
+from corpus import *
 """
 Simple in memory inverted index, based off of lectures
 Does not have partial indexing yet
 """
-def build_index(docs: dict()) -> dict():
-    index = dict()
+def build_index():
     doc_num = 0
+    mem_index_dict = dict()
     # TODO partial indexing, but don't need for milestone 1
         # while docs:
         #     batch = get_batch(docs)
-    for (url, doc) in docs.items():
+    for i in get_all_jsons():
         doc_num = doc_num + 1
-        tokens = parse(doc)
+        if doc_num % 10 == 0:
+            print(mem_index_dict)
+            yield mem_index_dict
+        tokens = parse(i)
         for token in tokens:
-            if token not in index:
-                index[token] = [Posting(doc_num, 1)]
+            if token not in mem_index_dict:
+                mem_index_dict[token] = [Posting(doc_num, 1)]
             else:
                 # if the current doc id is the same, update freqcount (no need to create new Posting object)
-                if (index[token][-1]).docid == doc_num:
-                    (index[token][-1]).tfidf = (index[token][-1]).tfidf + 1
+                if (mem_index_dict[token][-1]).docid == doc_num:
+                    (mem_index_dict[token][-1]).tfidf = (mem_index_dict[token][-1]).tfidf + 1
                 else:
-                    index[token].append(Posting(doc_num, 1))
-    return index
+                    mem_index_dict[token].append(Posting(doc_num, 1))
 
 
 """
@@ -47,3 +41,5 @@ def parse(text: str) -> list():
     for i in data:
         newList.append(i)
     return newList
+
+
