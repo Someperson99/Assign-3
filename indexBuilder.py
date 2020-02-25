bc5330cebcbc7d62886811b044672429ba950051
Allyson Yamasaki <allyson.yamasaki@gmail.com>
2020-02-24 16:08:50 -0800

    working i think, now using list instead of posting object

--- indexBuilder.py
+++ indexBuilder.py
@@ -2,6 +2,7 @@
 import re
 from Posting import *
 from corpus import *
+from json_handler import *
 """
 Simple in memory inverted index, based off of lectures
 Does not have partial indexing yet
@@ -13,24 +14,22 @@ def build_index():
         # while docs:
         #     batch = get_batch(docs)
     for i in get_all_jsons():
-        print(i)
         doc_num = doc_num + 1
         if doc_num % 10 == 0:
-            print(mem_index_dict)
             write_to_file(mem_index_dict)
             mem_index_dict = dict()
         tokens = parse(i[1])
         for token in tokens:
             if token not in mem_index_dict:
-                mem_index_dict[token] = [Posting(doc_num, 1)]
+                # WE ARE NOW USING LIST INSTEAD OF POSTING OBJECT
+                mem_index_dict[token] = [[doc_num, 1]]   # [DOCID, COUNT]
             else:
                 # if the current doc id is the same, update freqcount (no need to create new Posting object)
-                if (mem_index_dict[token][-1]).docid == doc_num:
-                    (mem_index_dict[token][-1]).tfidf = (mem_index_dict[token][-1]).tfidf + 1
+                if (mem_index_dict[token][-1])[0] == doc_num:
+                    (mem_index_dict[token][-1])[1] = (mem_index_dict[token][-1])[1] + 1
                 else:
-                    mem_index_dict[token].append(Posting(doc_num, 1))
+                    mem_index_dict[token].append([doc_num, 1])
     write_to_file(mem_index_dict)
-
 
 
 """
@@ -45,3 +44,5 @@ def parse(text: str) -> list():
     for i in data:
         newList.append(i)
     return newList
+
+build_index()
