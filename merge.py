a65a4ed6e6e43c9b03d2223ccf548607f1d4cb72
Someperson99 <39724421+Someperson99@users.noreply.github.com>
2020-03-02 22:28:09 -0800

    added function in merge

--- merge.py
+++ merge.py
@@ -2,4 +2,7 @@ import os
 import sys
 
 def get_paths():
-    pass
+    return sorted([file for file in os.listdir(os.getcwd()) if file.endswith(".json")],
+                  key = lambda x: (x[0], x[1:-5]))
+
+print(get_paths())
