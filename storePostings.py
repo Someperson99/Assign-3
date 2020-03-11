1b4662624c6a6e37deac5bfe753fc21b7c04440e
Someperson99 <39724421+Someperson99@users.noreply.github.com>
2020-03-10 20:18:48 -0700

    created functions to store and retrieve postings

--- storePostings.py
+++ storePostings.py
@@ -1,2 +1,18 @@
-def store_stream(stream:str):
-    pass
+import os
+
+def store_postings(postings_dict: dict):
+    for word, postings in postings_dict.items():
+        path = word + ".txt"
+        if not os.path.exists(word + ".txt"):
+            f = open(word + ".txt", 'w')
+            f.close()
+        postings_str = ""
+        for posting in postings:
+            postings_str += str(posting[0]) + ":" + str(posting[1]) + ","
+        with open(path, 'a') as f:
+            f.write(postings_str)
+
+
+def get_postings(word: str):
+    with open(word + ".txt", 'r') as f:
+        return f.readline()
