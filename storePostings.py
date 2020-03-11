04aecd12cac998fa59fae61dca91d6a986baa9fd
Someperson99 <39724421+Someperson99@users.noreply.github.com>
2020-03-10 21:24:00 -0700

    added ',' to get_posting split call

--- storePostings.py
+++ storePostings.py
@@ -3,9 +3,6 @@ import os
 def store_postings(postings_dict: dict):
     for word, postings in postings_dict.items():
         path = "C:\\Users\\geryj\Documents\\Index Copy\\"
-        if not os.path.exists(path + word + ".txt"):
-            f = open(path + word + ".txt", 'w')
-            f.close()
         postings_str = ""
         for posting in postings:
             postings_str += str(posting[0]) + ":" + str(posting[1]) + ","
@@ -16,6 +13,6 @@ def store_postings(postings_dict: dict):
 
 def get_postings(word: str):
     with open(word + ".txt", 'r') as f:
-        res = f.readline().split()
+        res = f.readline().split(',')
     f.close()
     return res
