0c883ec692be6a9be277877eb7482afa91b6f50d
Someperson99 <39724421+Someperson99@users.noreply.github.com>
2020-03-13 19:17:42 -0700

    copied from master

--- corpus.py
+++ corpus.py
@@ -30,15 +30,36 @@ def read_json_files(path: str) -> str:
     f = open(path, 'r')
     html_data = json.load(f)
     f.close()
+    important_tags = ['strong', 'b', 'header', 'p', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6']
     soup = BeautifulSoup(html_data['content'], features='html.parser')
+
+    content = ""
+
     try:
         title = soup.title.string.strip()
     except:
         title = ""
+    else:
+        title += " "
+        content += title * 4
 
-    content = ""
-    for para in soup.find_all('p'):
-        content += str(para.text)
+
+    for tag in important_tags:
+        try:
+            info = soup.find_all(tag)
+        except:
+            pass
+        else:
+            for word in info:
+                word += " "
+                if tag == "strong" or tag == "b":
+                    word = word * 3
+                elif tag == "header" or tag in important_tags[4:]:
+                    word = word * 2
+                else:
+                    pass
+                content += str(word)
+
 
     # LETS SAVE TITLE SEPARATELY SO WE CAN USE IT FOR THE GUI <3
     return (html_data['url'], title, content)
