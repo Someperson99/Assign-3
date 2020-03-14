c822b12da4ac60e8b5457e8250bc5a45f0546060
kae-ancheta <39729190+kae-ancheta@users.noreply.github.com>
2020-03-13 21:23:29 -0700

    handles queries with non alphanumeric characters

--- search.py
+++ search.py
@@ -1,88 +1,105 @@
-import os
-from json_handler import get_json_content
-from time import time
-import string
 import math
-from collections import OrderedDict
+import storePostings
+import json
+from nltk.stem import PorterStemmer
 
+stemmer = PorterStemmer()
+
+#depending on where you have urldict.json stored, you're going
+#to want to change this
+path = "/Users/allysonyamasaki/PycharmProjects/Assign-3/results/"
 
 def get_postings(word:str) -> list:
     '''given a word this function will find all postings from the index, assuming
     it has already been created'''
     if ("AND" in word or " "):
-        query = word.replace(" AND", "").lower().split()
+        q = word.replace(" AND", "").lower().split()
     else:
-        query = [word.lower()]
-    first_letter = list(q[0] for q in query)
-    index = 0
-    current_dir = os.getcwd()
-    all_postings = []
-    for letter in first_letter:
-        num = 0
-        postings = []
-        target_path = current_dir + "/" + letter + str(num) + ".json"
-        valid_path = os.path.exists(target_path)
-        while valid_path:
-            json_content = get_json_content(current_dir + "/" + letter + str(num) + ".json")
-            if query[index] in json_content:
-                for t in json_content[query[index]]:
-                    postings.append((t[0], t[1]))
-            num += 1
-            valid_path = os.path.exists(current_dir + "/" + letter + str(num) + ".json")
-        index += 1
-        all_postings.append(postings)
-    return all_postings
+        q = [word.lower()]
 
-def merge_postings(all_postings: list) -> dict:
+    query = []
+
+    for word in q:
+        tempWord = ""
+        c = 0
+        for char in word:
+            if len(word) > 1:
+                if char == "'" and c == word.rfind("'"):
+                    break
+            if char.isalnum():
+                tempWord += char
+            c += 1
+        if tempWord == "":
+            query.append(word)
+        else:
+            query.append(tempWord)
+
+    res = []
+
+    #for each token in the query
+    for token in query:
+        #retrieve all of the postings, put it into a list, and then
+        #put it into res
+        res.append(storePostings.get_postings(stemmer.stem(token)))
+
+    return res
+
+def merge_postings(all_postings: list) -> list:
     # need all_postings to be a list of lists of tuples
+    result = {}
     all_postings.sort(key = len)
-    t = dict(all_postings[0])
-    if len(all_postings) < 2:
-        return t
+    t = {}
+    for posting in all_postings[0]:
+        #this is dealing with only one list so posting
+        #is a string representation of a post, eg. '13:3'
+        #so that's why we have to split it
+        post = posting.split(':')
+        if not len(post) == 1:
+            docID = int(post[0])
+            frequency = int(post[1])
+            t[docID] = frequency
+    if len(all_postings) == 1: return t
     for postings in all_postings[1:]:
-        temp = dict(postings)
-        result = {}
+        #for the other lists that are in postings...
+        temp = {}
+        for posting in postings:
+            #doing the same thing here, just getting all of
+            #the posting information and putting it into
+            #temp
+            post = posting.split(':')
+            if not len(post) == 1:
+                docID = int(post[0])
+                frequency = int(post[1])
+                temp[docID] = frequency
+        #put all of the keys into a set, and compare them to
+        #t which holds the shortest amount of keys
         match = set(temp.keys()).intersection(t.keys())
         for key in match:
+            #add the postings that match up into result
             result[key] = temp[key] + t[key]
         t = result.copy()
-    print(len(result))
     return result
 
-x1 = time()
-print(merge_postings(get_postings("cristina")))
-x2 = time()
-
-print(x2-x1)
+print(get_postings("Christina"))
 
 def get_tfidf(postings_dict: dict) -> dict:
     """ get tfidf score given a word AT SEARCH TIME
         The tf-idfweight of a term is the product of its tfweight and its idfweight.
         wt,d= (1+log(tft,d)) x log(N/dft)
     """
+    #dictionary that will hold the docID as the key, and tf*idf as its value
     tfidf = dict()
-    current_dir = os.getcwd()
-    num = 0
-    num_documents = 0
-    for i in string.ascii_lowercase:
-        target_path = current_dir + "/" + i + str(num) + ".json"
-        valid_path = os.path.exists(target_path)
-        while valid_path:
-            json_content = get_json_content(current_dir + "/" + i + str(num) + ".json")
-            for word in json_content:
-                if int(json_content[word][-1][0]) > num_documents:
-                    num_documents = int(json_content[word][-1][0])
-            num += 1
-            valid_path = os.path.exists(current_dir + "/" + i + str(num) + ".json")
+    #over here we are just opening up the json file that contains all of the docIDs
+    #and extracting the largest docID from it, this will represent the number of files
+    #since the docID is essentially a counter of documents
+    with open(path + "urldict.json", 'r') as f:
+        res = json.load(f)
+        f.close()
+    num_documents = int(list(res.keys())[-1])
 
     document_frequency = len(postings_dict)  # num of documents that contain token
-    idf = math.log((num_documents / document_frequency), 10)
+    if document_frequency > 0:
+        idf = math.log((num_documents / document_frequency), 10)
     for key in postings_dict.keys():
         tfidf[key] = (1 + math.log(postings_dict[key], 10)) * idf
     return tfidf
-
-
-x3 = time()
-print(sorted(get_tfidf(merge_postings(get_postings("cristina machine learning"))).items(), key=lambda kv: kv[1], reverse=True))
-x4 = time()
-print(x4-x3)
